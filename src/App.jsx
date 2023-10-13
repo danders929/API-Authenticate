6af6d2225984c46dc6ee5e1bc6a87b26da93cbbc
Dan <sandersda@gmail.com>
2023-10-13 16:15:20 -0500

    Initialize version 1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Authenticate  from './components/Authenticate'
+import SignUpForm from './components/SignUpForm'
+
+import './App.css'
+
+function App() {
+  const [token, setToken] = useState(null)
+
+  return (
+    <>
+      <SignUpForm setToken={setToken} />
+      <Authenticate token={token} />
+    </>
+  )
+}
+
+export default App
